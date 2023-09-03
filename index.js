f752dd4b77bd7db86442c4fa0def956b1a9c4569
anilkarki2057 <anilkarki2057@gmail.com>
2023-09-03 20:07:19 +0545

    still not completed

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-console.log("going to learn express js");
-console.log("hellow testing");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json()); //middleware
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
+);
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.lenght - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send("Done");
+});
+
+app.listen(5000, () => {
+  console.log("App is running on port number 5000");
+});
